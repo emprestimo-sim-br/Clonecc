317b98cb69d3c4bf54d32238d49a2105c92d4e5f
snowfelps <123759120+snowfelps@users.noreply.github.com>
2023-10-02 23:13:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-# limpar 
+# Limpar
 clear
 echo -e "\e[1;31mExecutando instalação...\e[0m"
 
 echo "Este script solicitará permissões para o Termux acessar a pasta Android."
 
-# pedir permissão do storage
+# Pedir permissão do storage
 termux-setup-storage
 
 # Aceitar automaticamente a opção 'y'
 echo 'y' | termux-setup-storage
 
-# Esperar alguns segundos 
+# Esperar alguns segundos
 sleep 3
 
 # Instalar o Python 2
 pkg install python2 -y
 
+# Instalar o cmatrix
+pkg install cmatrix -y
+
 # Limpar a tela antes de executar o script Python
 clear
 
-# cor vermelha 
+# Cor vermelha
 echo -e "\e[1;31mExecutando script Python...\e[0m"
 
 # Obter o diretório atual
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# percorrer ao directorio absoluto.
+# Percorrer ao diretório absoluto.
 PYTHON_PATH="/data/data/com.termux/files/usr/bin/python2"
 
-# Executar o script Python 
+# Executar o script Python
 if [ -x "$PYTHON_PATH" ]; then
     "$PYTHON_PATH" "$DIR/hackertcp.py"
 else
